91668151959cbc44b4afb7b5e1396ee17fa171eb
Amber Johnson <amberjohnsonsmile@gmail.com>
2020-05-11 16:03:27 -0600

    Initial commit

---
+++ src/test/java/com/learncamel/learncamelspringboot/LearncamelSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learncamel.learncamelspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearncamelSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
