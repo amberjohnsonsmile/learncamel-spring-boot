bdf3e034647a4dab65540b5dbe7c91d21e928b9e
Amber Johnson <amberjohnsonsmile@gmail.com>
2020-05-13 15:54:36 -0600

    Adding more environment stuff

--- src/main/java/com/learncamel/learncamelspringboot/route/SimpleCamelRoute.java
+++ src/main/java/com/learncamel/learncamelspringboot/route/SimpleCamelRoute.java
@@ -1,15 +1,24 @@
 package com.learncamel.learncamelspringboot.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleCamelRoute extends RouteBuilder {
 
+    private final Environment environment;
+
+    public SimpleCamelRoute(Environment environment) {
+        this.environment = environment;
+    }
+
     public void configure() {
 
         from("{{startRoute}}")
-                .log("Timer Invoked and the body is ${body}")
+                .log("Timer Invoked and the body " + environment.getProperty("message"))
                 .pollEnrich("{{fromRoute}}")
                 .to("{{toRoute1}}");
     }
